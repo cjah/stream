d4df3a2d17000768697686181fa1f3f10ae78fa3
cjah <curtis.jah@gmail.com>
2020-09-24 11:48:24 -0700

    finished createStream action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
@@ -13,6 +13,8 @@ export const signOut = () => {
 	};
 };
 
-export const createStream = (formValues) => async dispatch => {
-	streams.post('/streams', formValues);
+export const createStream = formValues => async dispatch => {
+	const response = await streams.post('/streams', formValues);
+
+	dispatch({ type: CREATE_STREAM, payload response.data });
 };
