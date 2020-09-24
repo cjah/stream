92aeaba2d14070074b4c5f42658069608aa82377
cjah <curtis.jah@gmail.com>
2020-09-24 11:49:04 -0700

    fixed typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,5 +16,5 @@ export const signOut = () => {
 export const createStream = formValues => async dispatch => {
 	const response = await streams.post('/streams', formValues);
 
-	dispatch({ type: CREATE_STREAM, payload response.data });
+	dispatch({ type: CREATE_STREAM, payload: response.data });
 };
